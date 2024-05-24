639085b3e508081705d38074cacca9dc492d5211
Надира Тургуналиева <1132226224@pfur.ru>
2024-05-24 15:06:58 +0300

    ///

---
+++ 1/1.cpp
@@ -0,0 +1,73 @@
+﻿#include <iostream>
+#include <vector>
+#include <list>
+#include <chrono>
+
+using namespace std::chrono;
+using namespace std;
+
+void append_to_vector()
+{
+    vector<int> vec;
+    for (int i = 0; i < 1000; ++i) {
+        vec.push_back(i);
+    }
+}
+
+void append_to_list()
+{
+    list<int> lst;
+    for (int i = 0; i < 1000; ++i) {
+        lst.push_back(i);
+    }
+}
+
+void measure_duration()
+{
+    // Измеряем время выполнения для вектора
+    auto start_vector = high_resolution_clock::now();
+    for (int i = 0; i < 1000; ++i) {
+        append_to_vector();
+    }
+    auto end_vector = high_resolution_clock::now();
+    auto vector_duration = duration_cast<milliseconds>(end_vector - start_vector).count();
+
+    // Измеряем время выполнения для списка
+    auto start_list = high_resolution_clock::now();
+    for (int i = 0; i < 1000; ++i) {
+        append_to_list();
+    }
+    auto end_list = high_resolution_clock::now();
+    auto list_duration = duration_cast<milliseconds>(end_list - start_list).count();
+
+    // Выводим результаты
+    cout << "Время выполнения для вектора: " << vector_duration << " миллисекунд" << endl;
+    cout << "Время выполнения для списка: " << list_duration << " миллисекунд" << endl;
+}
+
+void measure_size()
+{
+    std::vector<int> vec(1000);  // Вектор из 1000 элементов
+    std::list<int> lst(1000);    // Список из 1000 элементов
+
+    // Размер вектора
+    size_t vector_memory = sizeof(vec) + (vec.capacity() * sizeof(int));
+
+    // Размер списка
+    size_t list_memory = sizeof(lst);
+    for (auto it = lst.begin(); it != lst.end(); ++it) {
+        list_memory += sizeof(*it);
+    }
+
+    std::cout << "Объем памяти, занимаемый вектором: " << vector_memory << " байт" << std::endl;
+    std::cout << "Объем памяти, занимаемый списком: " << list_memory << " байт" << std::endl;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+    measure_size();
+    measure_duration();
+
+    return 0;
+}
---
+++ 2/2.cpp
@@ -0,0 +1,68 @@
+﻿#include <iostream>
+#include <vector>
+#include <list>
+#include <chrono>  // для изменения времени
+
+using namespace std;
+using namespace std::chrono;
+
+void test_insert(int n) {
+    list<int> l;
+    vector<int> v;
+
+    //измерение времени для вставки элементов в список
+
+    // тип auto позволяет компилятору самостоятельно определить тип переменной 
+    // на основе ее инициализации
+    auto start = high_resolution_clock::now();   //записывается текущее время
+    for (int i = 0; i < n; i++) {                //вставка элементов
+        l.push_back(i);
+    }
+    auto end = high_resolution_clock::now();
+    cout << "Вставка в список: " << duration_cast<microseconds>(end - start).count() << " мкс" << endl;
+    // duration_cast для преобразования значения продолжительности из одного типа в другой
+    //Метод count() извлекает количество микросекунд из объекта std::chrono::microseconds
+
+    start = high_resolution_clock::now();
+    for (int i = 0; i < n; i++) {
+        v.push_back(i);
+    }
+    end = high_resolution_clock::now();
+    cout << "Вставка в вектор: " << duration_cast<microseconds>(end - start).count() << " мкс" << endl;
+}
+
+void test_erase(int n) {
+    list<int> l;
+    vector<int> v;
+
+    for (int i = 0; i < n; i++) {
+        l.push_back(i);
+        v.push_back(i);
+    }
+
+    auto start = high_resolution_clock::now();
+    for (int i = 0; i < n; i++) {
+        l.pop_back();
+    }
+    auto end = high_resolution_clock::now();
+    cout << "Удаление из списка: " << duration_cast<microseconds>(end - start).count() << " мкс" << endl;
+
+    start = high_resolution_clock::now();
+    for (int i = 0; i < n; i++) {
+        v.pop_back();
+    }
+    end = high_resolution_clock::now();
+    cout << "Удаление из вектора: " << duration_cast<microseconds>(end - start).count() << " мкс" << endl;
+}
+
+int main() {
+
+    setlocale(LC_ALL, "RU");
+    int n = 1000000;
+
+    test_insert(n);
+    cout << endl;
+    test_erase(n);
+
+    return 0;
+}
